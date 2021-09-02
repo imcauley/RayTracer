b9851e4a289df976b5d82b2902d6584d6d74663d
Isaac <mcauleyisaac@gmail.com>
2021-09-01 23:35:43 -0600

    added point object

---
+++ src/point.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Point:
+	def __init__(self, vector):
+		self.vector = vector
+		
+	def copy(self):
+		return np.array(self.vector)
+		
+	def translate(self, point):
+		self.vector = self.vector - point.vector
+		
+	def x_rotation(self, theta):
+		R = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0, np.sin(theta), np.cos(theta)]])
+		self.vector = np.dot(R,self.vector)
+
+	def y_rotation(self, theta):
+		R = np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta), 0, np.cos(theta)]])
+		self.vector = np.dot(R,self.vector)
+	
+	def z_rotation(self, theta):
+		R = np.array([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
+		self.vector = np.dot(R,self.vector)
