c05fcf4ec42108ca9f4d31abfca736a24222c784
Isaac <mcauleyisaac@gmail.com>
2021-09-03 22:52:47 -0600

    added intersect tests

---
+++ src/tests/intersects.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+sys.path.append('..')
+
+from sphere import Sphere
+from ray import Ray
+import numpy as np
+
+class TestIntersects(unittest.TestCase):
+    def test_sphere_hit(self):
+        r = Ray(np.array([0, 0, 0], dtype='f'), np.array([0, 1, 0], dtype='f'), dtype='f')
+        s = Sphere(np.array([0, 3, 0], dtype='f'), 1)
+
+        self.assertIsNotNone(s.intersect(r))
+
+    def test_sphere_miss(self):
+        r = Ray(np.array([0, 0, 0], dtype='f'), np.array([0, -1, 0], dtype='f'))
+        s = Sphere(np.array([0, 3, 0], dtype='f'), 1)
+
+        self.assertIsNone(s.intersect(r))
+
+if __name__ == '__main__':
+    unittest.main()
