77e0763371f618892fa5157032791a27b63eb6b8
Isaac <mcauleyisaac@gmail.com>
2021-09-11 15:55:03 -0600

    Fixed import errors

--- src/raytracer/__init__.py
+++ src/raytracer/__init__.py
@@ -8,3 +8,4 @@ from .light import Light
 
 from .intersect_point import IntersectPoint
 from .object import Object
+from .sphere import Sphere
--- src/raytracer/camera.py
+++ src/raytracer/camera.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from raytracer import Point
+from raytracer import Point, Ray
 
 @dataclass
 class Camera:
@@ -13,31 +13,32 @@ class Camera:
     width_fov: float = 1.5707963268
 
     def create_rays(self):
-        width_step = self.width / self.width_pov
-        height_step = self.height / self.height_pov
+        width_step = self.width / self.width_fov
+        height_step = self.height / self.height_fov
         
         start = self.direction.copy()
         start.translate(self.position)
-        start.rotate_y(-self.height_pov / 2)
-        start.rotate_x(-self.width_pov / 2)
+        start.y_rotation(-self.height_fov / 2)
+        start.x_rotation(-self.width_fov / 2)
         
         rays = []
         
         for i in range(0, self.height):
             row_start = start.copy()
-            row_start.rotate_y(i * height_step)
+            row_start.y_rotation(i * height_step)
             
             for j in range(0, self.width):
                 current = row_start.copy()
-                current.rotate_x(j * width_step)
-                rays.append(current)
+                current.x_rotation(j * width_step)
+                r = Ray(self.position, current)
+                rays.append(r)
                 
                 
         self.rays = rays
 
     def create_image(self, objects, lights):
         brightnesses = []
-        for (index, ray) in enumerate(self.rays):
+        for ray in self.rays:
             b = ray.colour_from_ray(objects, lights)
             brightnesses.append(b)
 
