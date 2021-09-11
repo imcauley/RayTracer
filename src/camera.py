25dc2018f9e951c5ebbebd0c619163ab921c9ef8
Isaac <mcauleyisaac@gmail.com>
2021-09-10 23:32:44 -0600

    Added camera object

--- src/camera.py
+++ src/camera.py
@@ -3,38 +3,43 @@ from point import Point
 
 @dataclass
 class Camera:
-	width: int
-	height: int
-	
-	position: Point
-	direction: Point
-	
-	height_fov: float = 1.5707963268
-	width_fov: float = 1.5707963268
-	
-	def create_rays(self):
-		width_step = self.width / self.width_pov
-		height_step = self.height / self.height_pov
-		
-		start = self.direction.copy()
-		start.translate(self.position)
-		start.rotate_y(-self.height_pov / 2)
-		start.rotate_x(-self.width_pov / 2)
-		
-		rays = []
-		
-		for i in range(0, self.height):
-			row_start = start.copy()
-			row_start.rotate_y(i * height_step)
-			
-			for j in range(0, self.width):
-				current = row_start.copy()
-				current.rotate_x(j * width_step)
-				rays.append(current)
-				
-				
-		self.rays = rays
-		
-	def create_image(self, objects, lights):
-		for ray in self.rays:
-			ray.intercepting_point()
+    width: int
+    height: int
+
+    position: Point
+    direction: Point
+
+    height_fov: float = 1.5707963268
+    width_fov: float = 1.5707963268
+
+    def create_rays(self):
+        width_step = self.width / self.width_pov
+        height_step = self.height / self.height_pov
+        
+        start = self.direction.copy()
+        start.translate(self.position)
+        start.rotate_y(-self.height_pov / 2)
+        start.rotate_x(-self.width_pov / 2)
+        
+        rays = []
+        
+        for i in range(0, self.height):
+            row_start = start.copy()
+            row_start.rotate_y(i * height_step)
+            
+            for j in range(0, self.width):
+                current = row_start.copy()
+                current.rotate_x(j * width_step)
+                rays.append(current)
+                
+                
+        self.rays = rays
+
+    def capture_rays(self, objects, lights):
+        self.ray_color
+        for ray in self.rays:
+            self.intercepted_points.append(ray.intercepting_point())
+
+
+    def create_image(self, image_creator):
+        return image_creator.image_from_points(self.intercepted_points)
