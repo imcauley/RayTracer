d85a3e2712173bb7a5f740d217d490df1e8e1f80
Isaac <mcauleyisaac@gmail.com>
2021-09-13 21:29:30 -0600

    Fixed tests

--- src/tests/intersects.py
+++ src/tests/intersects.py
@@ -2,20 +2,36 @@ import unittest
 import sys
 sys.path.append('./src')
 
-from sphere import Sphere
-from ray import Ray
+from raytracer import Sphere
+from raytracer import Ray
+from raytracer import Point
+
 import numpy as np
 
 class TestIntersects(unittest.TestCase):
     def test_sphere_hit(self):
-        r = Ray(np.array([0, 0, 0], dtype='f'), np.array([0, 1, 0], dtype='f'))
-        s = Sphere(np.array([0, 3, 0], dtype='f'), 1)
+        r = Ray(
+            Point(np.array([0, 0, 0], dtype='f')), 
+            Point(np.array([0, 1, 0], dtype='f'))
+            )
+        s = Sphere(
+            np.array([0, 3, 0], dtype='f'), 
+            np.array([0.5, 0.5, 0], dtype='f'), 
+            1
+            )
 
         self.assertIsNotNone(s.intersect(r))
 
     def test_sphere_miss(self):
-        r = Ray(np.array([0, 0, 0], dtype='f'), np.array([0, -1, 0], dtype='f'))
-        s = Sphere(np.array([0, 3, 0], dtype='f'), 1)
+        r = Ray(
+            Point(np.array([0, 0, 0], dtype='f')), 
+            Point(np.array([0, -1, 0], dtype='f'))
+            )
+        s = Sphere(
+            np.array([0, 3, 0], dtype='f'), 
+            np.array([0.5, 0.5, 0], dtype='f'), 
+            1
+            )
 
         i = s.intersect(r)
         self.assertIsNone(i)
