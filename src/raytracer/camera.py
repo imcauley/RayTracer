a2af5e3a566ca03db231dd0f6248ca58765f3aa8
Isaac <mcauleyisaac@gmail.com>
2021-09-16 17:15:06 -0600

    Fixed bug when creating rays

--- src/raytracer/camera.py
+++ src/raytracer/camera.py
@@ -16,23 +16,24 @@ class Camera:
     width_fov: float = 1.5707963268
 
     def create_rays(self):
-        width_step = self.width / self.width_fov
-        height_step = self.height / self.height_fov
+        width_step = self.width_fov / self.width
+        height_step = self.height_fov / self.height
         
         start = self.direction.copy()
         start.translate(self.position)
-        start.y_rotation(-self.height_fov / 2)
+        start.z_rotation(-self.height_fov / 2)
         start.x_rotation(-self.width_fov / 2)
+
+        offset = Point(start.vector * -1.0)
         
         rays = []
         
-        for i in range(0, self.height):
-            row_start = start.copy()
-            row_start.y_rotation(i * height_step)
-            
+        for i in range(0, self.height):            
             for j in range(0, self.width):
-                current = row_start.copy()
+                current = start.copy()
+                current.z_rotation(i * height_step)
                 current.x_rotation(j * width_step)
+                current.translate(offset)
                 r = Ray(self.position, current)
                 rays.append(r)
                 
@@ -45,12 +46,13 @@ class Camera:
         brightnesses = np.zeros([self.width, self.height, 3], 'f')
         for ray in self.rays:
             b = ray.colour_from_ray(objects, lights)
-            brightnesses[row, col] = Camera.brightness_to_rgb(b)
+            brightnesses[row, col] = b
             
             row += 1
             if row >= self.width:
                 row = 0
                 col += 0
+
 
         brightnesses = brightnesses * 255
         brightnesses = brightnesses.astype(np.uint8)
