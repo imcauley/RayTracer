56e9844a2158f677a248815e57b7bef7345c2dd1
Isaac <mcauleyisaac@gmail.com>
2021-09-24 16:09:26 -0600

    Added more ray tests

--- src/tests/ray.py
+++ src/tests/ray.py
@@ -9,11 +9,43 @@ import math
 import numpy as np
 
 class TestIntersects(unittest.TestCase):
+    def test_x_rotation(self):
+        p = Point(np.array([2, 0, 0], dtype='f'))
+        p.y_rotation(math.pi)
+
+        self.assertAlmostEqual(p.vector[0], -2)
+        self.assertAlmostEqual(p.vector[1], 0)
+        self.assertAlmostEqual(p.vector[2], 0)
+
+    def test_y_rotation(self):
+        p = Point(np.array([0, 2, 0], dtype='f'))
+        p.y_rotation(math.pi)
+
+        self.assertAlmostEqual(p.vector[0], 0)
+        self.assertAlmostEqual(p.vector[1], -2)
+        self.assertAlmostEqual(p.vector[2], 0)
+
     def test_z_rotation(self):
-        p = Point(np.array([0, 1, 0], dtype='f'))
+        p = Point(np.array([0, 0, 2], dtype='f'))
         p.z_rotation(math.pi)
 
-        self.assertAlmostEqual(p.vector[1], -1)
+        self.assertAlmostEqual(p.vector[0], 0)
+        self.assertAlmostEqual(p.vector[1], 0)
+        self.assertAlmostEqual(p.vector[2], -2)
+
+    def test_half_rotation(self):
+        p = Point(np.array([1, 0, 0], dtype='f'))
+        p.z_rotation(math.pi/2)
+
+        self.assertAlmostEqual(p.vector[0], 0)
+        self.assertAlmostEqual(p.vector[2], -1)
+
+    def test_quater_rotation(self):
+        p = Point(np.array([1, 0, 0], dtype='f'))
+        p.z_rotation(math.pi/4)
+
+        self.assertAlmostEqual(p.vector[0], 0.70710678)
+        self.assertAlmostEqual(p.vector[2], -0.70710678)
 
 if __name__ == '__main__':
     unittest.main()
