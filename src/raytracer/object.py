Fixed colour calculation

src/raytracer/object.py
@@ -27,5 +27,6 @@ class Object(ABC):
     def create_intersect_point(self, ray, distance) -> IntersectPoint:
         point = Point(np.add(ray.origin.vector, (distance * ray.direction.vector)))
         reflection = self.reflection(ray, point)
+        normal = self.normal_at_point(point)
 
-        return IntersectPoint(point, reflection, distance, self)
+        return IntersectPoint(point, reflection, normal, distance, self)

src/raytracer/ray.py
@@ -26,7 +26,7 @@ class Ray:
 
         brightness = 0
         for light in lights:
-            brightness += np.dot(light.position.vector, intersecting_point.reflection.vector())
+            brightness += np.dot(light.position.vector, intersecting_point.normal.vector)
 
         return intersecting_point.intersecting_object.colour * brightness
 